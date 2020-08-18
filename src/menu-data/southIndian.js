1c91e453cdd581442d39497ad352d39cb8022707
Rahul Raj <rahulraz1308@gmail.com>
2020-08-18 17:53:53 +0530

    added type in menudata

--- src/menu-data/southIndian.js
+++ src/menu-data/southIndian.js
@@ -2,49 +2,57 @@ const SouthIndianList = [{
     "name": "Masala Dosa",
     "price": [180],
     "isVeg": true,
-    "rating": 4.5
+    "rating": 4.5,
+    "type":"South Indian"
   },
   {
     "name": "Classic Idli",
     "price": [90],
     "isVeg": true,
-    "rating": 4.5
+    "rating": 4.5,
+    "type":"South Indian"
   },
   {
     "name": "Sambar Vadai",
     "price": [120],
     "isVeg": true,
-    "rating": 4.5
+    "rating": 4.5,
+    "type":"South Indian"
   },
   {
     "name": "Rava Upma",
     "price": [110],
     "isVeg": true,
-    "rating": 4.5
+    "rating": 4.5,
+    "type":"South Indian"
   },
   {
     "name": "Idiyappam",
     "price": [100],
     "isVeg": true,
-    "rating": 4.5
+    "rating": 4.5,
+    "type":"South Indian"
   },
   {
     "name": "Tomato Uthappam",
     "price": [180],
     "isVeg": true,
-    "rating": 4.5
+    "rating": 4.5,
+    "type":"South Indian"
   },
   {
     "name": "Rasam Rice",
     "price": [120],
     "isVeg": true,
-    "rating": 4.5
+    "rating": 4.5,
+    "type":"South Indian"
   },
   {
     "name": "Lemon Rice",
     "price": [90],
     "isVeg": true,
-    "rating": 4.5
+    "rating": 4.5,
+    "type":"South Indian"
   },
   {
     "name": "Dahi Vada",
